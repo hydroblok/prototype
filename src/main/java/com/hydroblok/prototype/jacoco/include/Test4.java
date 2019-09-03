1d7d4131448137e62a7cf7f89221d7e50438951e
Jimmy Luo <jluo@natera.com>
2019-09-03 13:10:20 -0700

    Add Test4
    #1w56t0[in progress]

---
+++ src/main/java/com/hydroblok/prototype/jacoco/include/Test4.java
@@ -0,0 +1,10 @@
+package com.hydroblok.prototype.jacoco.include;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test4 {
+    public String test() {
+        return "test4";
+    }
+}
